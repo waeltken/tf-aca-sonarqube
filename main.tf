b749ad6cc5f1ff896d483938452378bdd70cf38a
Clemens Wältken <clemens.waltken@microsoft.com>
2025-06-03 15:42:49 +0200

    Create and inject ACA into VNet

--- main.tf
+++ main.tf
@@ -15,6 +15,35 @@ resource "azurerm_resource_group" "main" {
   location = "West Europe"
 }
 
+resource "azurerm_virtual_network" "main" {
+  name                = "sonarqube-vnet"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/22"]
+}
+
+resource "azurerm_subnet" "aca" {
+  name                 = "sonarqube-aca-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "aca-delegation"
+    service_delegation {
+      name    = "Microsoft.App/environments"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "endpoints" {
+  name                 = "sonarqube-endpoints-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_log_analytics_workspace" "main" {
   name                = "sonarqube-log-analytics"
   location            = azurerm_resource_group.main.location
@@ -42,6 +71,7 @@ resource "azurerm_container_app_environment" "main" {
     maximum_count         = 1
   }
 
+  infrastructure_subnet_id   = azurerm_subnet.aca.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
 }
 
